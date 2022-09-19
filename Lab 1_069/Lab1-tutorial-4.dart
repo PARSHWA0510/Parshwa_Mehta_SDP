2a75d08a0e40d1f4007feaa5d30ab9097015a3cc
Parshwa Mehta <86891879+PARSHWA0510@users.noreply.github.com>
2022-09-19 22:53:31 +0530

    Add files via upload

---
+++ Lab 1_069/Lab1-tutorial-4.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+/*
+//program 1
+void main() {
+  print('Welcome everyone we will start learning flutter and dart !!');
+}
+//output:Welcome everyone we will start learning flutter and dart !!
+*/
+
+
+/*
+//program 2
+void main(){
+  int i;
+  for(i = -1;i < 5;i++)
+    print("Count is ${i + 1}");
+}
+*/
+/*output:
+Count is 0
+Count is 1
+Count is 2
+Count is 3
+Count is 4
+Count is 5*/
+
+
+/*
+//program 3
+void main() {
+  stdout.write('without new line ');
+  print('hello world');
+  print(calculate());
+}
+
+int calculate(){
+  return 4 * 2;
+}
+//output:without new line hello world
+         8
+//print() function puts \n at the end of line but stdout.write doesn't puts \n at end of line.
+*/
